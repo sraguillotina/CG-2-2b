f8fb07a5e425dd961220a81a88aeb6ffc721f77b
Al-Khwarizmi <nfg214@icloud.com>
2018-07-20 23:47:05 -0500

    Add Program.cs

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Class2_HelloWorldGreet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, what is your name?");
+            Console.Write("Type your name: ");
+            string name = Console.ReadLine();
+
+            Console.WriteLine();
+
+            Console.WriteLine("Hello, " + name + ".");
+            Console.ReadLine();
+
+        }
+    }
+}
