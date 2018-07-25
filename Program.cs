02f89b144627435f775458efd030ec02f2a1e72b
Al <nfg214@icloud.com>
2018-07-25 09:51:14 -0500

    Add Program.cs

--- Program.cs
+++ Program.cs
@@ -1,18 +1,28 @@
 ﻿using System;
 
-namespace Class2_HelloWorldGreet
+namespace Class2_Rectangle
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, what is your name?");
-            Console.Write("Type your name: ");
-            string name = Console.ReadLine();
+            Console.WriteLine("Hi, there. Let me show you what I can do. " +
+                              "Allow me to calculate the area of a rectangle for you." +
+                              " Please provide a positive, whole number for each dimension: Length and width.");
+            //Console.ReadLine();
 
-            Console.WriteLine();
+            Console.Write("Please type the length of your rectangle: ");
+            string x = Console.ReadLine();
 
-            Console.WriteLine("Hello, " + name + ".");
+            int x1 = int.Parse(x);
+
+            Console.Write("Please type the width of your rectangle: ");
+            string y = Console.ReadLine();
+
+            int y1 = int.Parse(y);
+
+            var z = x1 * y1;
+            Console.WriteLine("The area of your rectangle is: " + z );
             Console.ReadLine();
 
         }
